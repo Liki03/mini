14f258dd30eacdb01fdac02dab67022ada178320
Liki03 <likitha04ak@gmail.com>
2025-12-03 15:15:13 +0530

    First commit

---
+++ src/test/java/github/GithubPracticeTest.java
@@ -0,0 +1,14 @@
+package github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GithubPracticeTest {
+	@Test 
+	public void githubPracTest() {
+		Reporter.log("HiGithub",true);
+//		String BROWSER = System.getProperty("browser");
+//		System.out.println(BROWSER);
+	}
+
+}
---
+++ src/test/java/maven/MavenPracticeTest.java
@@ -0,0 +1,12 @@
+package maven;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class MavenPracticeTest {
+	@Test
+	public void mavenPracgticeTest() {
+		Reporter.log("Hi Maveen",true);
+	}
+
+}
